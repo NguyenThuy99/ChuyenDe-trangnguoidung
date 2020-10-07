03970562d7dd8c5378c6efc63368d5c0c313165e
NguyenThuy99 <70498669+NguyenThuy99@users.noreply.github.com>
2020-10-07 23:40:14 +0700

    update

--- src/app/trangchu/tintuc/tintuc.component.ts
+++ src/app/trangchu/tintuc/tintuc.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { BaseComponent } from 'src/app/lib/base.component';
 
 @Component({
   selector: 'app-tintuc',
   templateUrl: './tintuc.component.html',
   styleUrls: ['./tintuc.component.css']
 })
-export class TintucComponent implements OnInit {
+export class TintucComponent extends BaseComponent implements OnInit {
+  public tintucs;
 
-  constructor() { }
+  constructor(injector: Injector) { 
+    super(injector);
+  }
 
   ngOnInit(): void {
+    console.log(123)
+      this._api.get('/api/TinTucs').toPromise()
+      .then(res => {
+        this.tintucs = res;
+      }).catch(err => console.error(err));
   }
 
 }
