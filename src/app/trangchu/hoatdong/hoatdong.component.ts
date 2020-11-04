b3b5271f04cbb9ef7ac0ac16ef33e1a825d93878
NguyenThuy99 <70498669+NguyenThuy99@users.noreply.github.com>
2020-11-04 17:35:20 +0700

    update

--- src/app/trangchu/hoatdong/hoatdong.component.ts
+++ src/app/trangchu/hoatdong/hoatdong.component.ts
@@ -1,4 +1,5 @@
 import { Component,Injector, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { BaseComponent } from 'src/app/lib/base.component';
 
@@ -9,7 +10,7 @@ import { BaseComponent } from 'src/app/lib/base.component';
 })
 export class HoatdongComponent extends BaseComponent implements OnInit {
   quangcao: any;
-  constructor(injector: Injector) {
+  constructor(injector: Injector, private _sanitizer: DomSanitizer) {
     super(injector);
   }
 
@@ -18,6 +19,12 @@ export class HoatdongComponent extends BaseComponent implements OnInit {
       this._api.get('api/quangcao/get-all-quangcao'),
     ).takeUntil(this.unsubscribe).subscribe(res => {
       this.quangcao = res[0];
+
+      this.quangcao.map((res: any) => {
+        res.video = this._sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/" + res.video);
+        return res;
+      });
+      
       setTimeout(() => {
         this.loadScripts();
       });
